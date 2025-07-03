a2989c8389e85bf6fd7b352a4f17424ad738aab2
Jean Philippe Niyitegeka <j.niyitegek@alustudent.com>
2025-07-03 10:46:41 +0200

    the api project

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,8 +13,10 @@ if __name__ == "__main__":
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        employee_id)
 
     user_resp = requests.get(user_url)
     todos_resp = requests.get(todos_url)
